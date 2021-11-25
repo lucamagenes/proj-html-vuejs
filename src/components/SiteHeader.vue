baba03290d7f9e42f7d9c63fab98a0443c956afc
lucamagenes <lucamgns33@gmail.com>
2021-11-25 18:33:32 +0100

    complete SiteHeader component

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,20 +1,43 @@
 <template>
-  <header>
-    <nav class="container d-flex">
-      <div class="logo">
-        <img
-          src="../assets/img/logotype.png"
-          alt="logo-brand-name"
-          height="20"
-        />
+  <div class="hero">
+    <header>
+      <nav class="container d-flex">
+        <div class="logo">
+          <img
+            src="../assets/img/logotype.png"
+            alt="logo-brand-name"
+            height="20"
+          />
+        </div>
+        <div class="right d-flex">
+          <div class="navLink" v-for="link in navLink" :key="link">
+            <a href="#">{{ link }} </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <div class="call-to container">
+      <div class="small-title d-flex">
+        <div class="green-line"></div>
+        <p>ALWAYS THE BEST WAY YOU NEED IT</p>
       </div>
-      <div class="right d-flex">
-        <div class="navLink" v-for="link in navLink" :key="link">
-          <a href="#">{{ link }} </a>
+      <div class="title">
+        <h1>
+          THE BEST <br />
+          BUSINESS <br />
+          CONSULTING
+        </h1>
+      </div>
+      <div class="btn-group">
+        <div class="read-more">
+          <button class="btn">READ MORE</button>
+        </div>
+        <div class="play-btn">
+          <button>PLAY</button>
         </div>
       </div>
-    </nav>
-  </header>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -30,18 +53,72 @@ export default {
 <style lang="scss">
 @import "../assets/scss/common.scss";
 
-nav {
-  height: 40px;
-  justify-content: space-between;
-  align-items: flex-end;
+.hero {
+  background-image: url("../assets/img/portrait-of-young-architect-woman-on-meeting-KFZCE3A.jpg");
+  background-repeat: no-repeat;
+  background-position-x: right;
+  background-size: 65%;
+  header {
+    nav {
+      height: 40px;
+      justify-content: space-between;
+      align-items: flex-end;
 
-  a {
-    margin-left: 2rem;
-    text-decoration: none;
-    color: $text-not-hover;
+      a {
+        margin-left: 2rem;
+        text-decoration: none;
+        color: $text-not-hover;
+      }
+      a:hover {
+        color: $text-primry;
+      }
+    }
   }
-  a:hover {
-    color: $text-primry;
+  .call-to {
+    padding-top: 200px;
+    padding-bottom: 200px;
+
+    .small-title {
+      align-items: flex-end;
+      margin-bottom: 2rem;
+      .green-line {
+        width: 45px;
+        height: 1px;
+        border: 1px solid #35ffbf;
+        margin-right: 1rem;
+      }
+      p {
+        color: $text-primry;
+        vertical-align: baseline;
+      }
+    }
+    .title {
+      color: $text-primry;
+      h1 {
+        font-size: 95px;
+        line-height: 80px;
+      }
+    }
+    .btn-group {
+      margin-top: 2rem;
+      display: flex;
+
+      .read-more {
+        margin-right: 2rem;
+        button {
+          background-color: $button-green;
+        }
+      }
+      .play-btn {
+        button {
+          padding: 1rem;
+          border: 1px solid white;
+          color: $text-green;
+          background-color: transparent;
+          border-radius: 100%;
+        }
+      }
+    }
   }
 }
 </style>
