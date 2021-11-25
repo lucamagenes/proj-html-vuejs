db63afb5107df7bb3a66bcb06e6b7beb12c829fe
lucamagenes <lucamgns33@gmail.com>
2021-11-25 17:32:48 +0100

    header component complete

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,10 +1,47 @@
 <template>
-  <header></header>
+  <header>
+    <nav class="container d-flex">
+      <div class="logo">
+        <img
+          src="../assets/img/logotype.png"
+          alt="logo-brand-name"
+          height="20"
+        />
+      </div>
+      <div class="right d-flex">
+        <div class="navLink" v-for="link in navLink" :key="link">
+          <a href="#">{{ link }} </a>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      navLink: ["HOME", "ABOUT", "PROJECTS", "SERVICES", "BLOG", "CONTACT"],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
+@import "../assets/scss/common.scss";
+
+nav {
+  height: 40px;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  a {
+    margin-left: 2rem;
+    text-decoration: none;
+    color: $text-not-hover;
+  }
+  a:hover {
+    color: $text-primry;
+  }
+}
 </style>
