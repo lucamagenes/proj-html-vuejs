5cd9ae54e77acde4b6d56b63280fe22a5e74e5c0
lucamagenes <lucamgns33@gmail.com>
2021-11-29 13:04:47 +0100

    create footertop complete sitefooter and footertop add change on variables scss

---
+++ src/components/FooterTop.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="footerTop">
+    <div class="container">
+      <div class="row d-flex">
+        <div class="logo">
+          <a href="#">
+            <img src="../assets/img/logotype-2-150x25.png" alt="logotype-2" />
+          </a>
+        </div>
+        <div class="menu">
+          <a href="">HOME</a>
+          <a href="">ABOUT</a>
+          <a href="">PROJECTS</a>
+          <a href="">SERVICES</a>
+          <a href="">BLOG</a>
+          <a href="">CONTACT</a>
+        </div>
+        <div class="icons">
+          <a href="">INSTAGRAM</a>
+          <a href="">LINKEDIN</a>
+          <a href="">FACEBOOK</a>
+          <a href="">TWITTER</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/common.scss";
+
+.menu {
+  a {
+    font-size: 1rem;
+    text-decoration: none;
+    color: $text-footer;
+    padding: 0 0.5rem;
+  }
+  a:hover {
+    color: white;
+  }
+}
+.icons {
+  a {
+    font-size: 1rem;
+    text-decoration: none;
+    color: white;
+    padding: 0 0.5rem;
+  }
+}
+</style>
--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,10 +1,97 @@
 <template>
-  <footer></footer>
+  <footer>
+    <FooterTop />
+    <div class="container">
+      <hr />
+      <div class="row d-flex">
+        <div class="termAndPrivacy">
+          <a href="#">Term of use</a>
+          <a href="#">Privacy Environmental Policy</a>
+        </div>
+        <div class="copyright">
+          <p>Copyright © 2020 Phlox Consulting. All Right Reserved.</p>
+        </div>
+      </div>
+    </div>
+    <div class="btn-group d-flex">
+      <div class="up">
+        <a href="#">UP</a>
+      </div>
+      <div class="message">
+        <a href="#">MSG</a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {};
+import FooterTop from "./FooterTop.vue";
+export default {
+  components: {
+    FooterTop,
+  },
+};
 </script>
 
 <style lang="scss">
+@import "../assets/scss/common.scss";
+footer {
+  color: $text-footer;
+  position: relative;
+  hr {
+    border-color: #787887;
+  }
+  .row {
+    justify-content: space-between;
+    align-items: flex-end;
+    margin: 3rem 0;
+    font-size: 0.8rem;
+    padding: 0 0.5rem;
+    .termAndPrivacy {
+      a {
+        display: block;
+        text-decoration: none;
+        color: $text-footer;
+      }
+      a:hover {
+        color: white;
+      }
+    }
+  }
+  .btn-group {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    padding-right: 0.5rem;
+    gap: 0.5rem;
+    align-items: center;
+    a {
+      text-decoration: none;
+      vertical-align: middle;
+    }
+    .up {
+      width: 25px;
+      height: 25px;
+      line-height: 25px;
+      text-align: center;
+      background-color: white;
+      border: 0.5px solid black;
+      border-radius: 100%;
+      a {
+        color: black;
+      }
+    }
+    .message {
+      width: 40px;
+      height: 40px;
+      line-height: 40px;
+      text-align: center;
+      background-color: $background-chat-icon;
+      border-radius: 100%;
+      a {
+        color: white;
+      }
+    }
+  }
+}
 </style>
